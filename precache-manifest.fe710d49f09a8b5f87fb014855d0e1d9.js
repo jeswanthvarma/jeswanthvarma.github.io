20f16d38c12fb5a5a1a3b1e019a5c0d39282476e
Jeswanth <jeswanth.m@techsophy.com>
2020-08-28 11:56:36 +0530

    React 30days app build

---
+++ precache-manifest.fe710d49f09a8b5f87fb014855d0e1d9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcf05df761f6b27a02b0034332fcc09d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54b952bc3854707c5ed2",
+    "url": "/static/css/main.86ad5824.chunk.css"
+  },
+  {
+    "revision": "5fddd8c559282de2f8ec",
+    "url": "/static/js/2.9cf9d6f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9cf9d6f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54b952bc3854707c5ed2",
+    "url": "/static/js/main.5d2ea302.chunk.js"
+  },
+  {
+    "revision": "87ba52dd0798248e910a",
+    "url": "/static/js/runtime-main.9430853a.js"
+  },
+  {
+    "revision": "f0d451a1160ef8f3f48317cfaea27517",
+    "url": "/static/media/doug.f0d451a1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "7725ae5d823e1db638f8bbfa57103d40",
+    "url": "/static/media/fontawesome-webfont.7725ae5d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
